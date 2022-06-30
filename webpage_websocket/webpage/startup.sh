bb70561cd80b281a5be1811075dfd9f644780f97
nakashimn <nakashimn@gmail.com>
2022-06-30 21:25:01 +0900

    delete:webpage_websocket

--- webpage_websocket/webpage/startup.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-python ./manage.py makemigrations
-python ./manage.py migrate
-nohup uwsgi --socket :8001 --module webpage.wsgi --py-autoreload 1 --logto /tmp/mylog.log & daphne -b 0.0.0.0 -p 3001 --ping-interval 10 --ping-timeout 120 webpage.asgi:application
